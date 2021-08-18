10c9e5887e02b304e3fae62e77aa7ee854cdce45
Yaseer Anaisi Okino <37089367+yaseerokino@users.noreply.github.com>
2021-08-18 09:22:01 +0100

    Update functions to to class methods

--- index.js
+++ index.js
@@ -1,12 +1,56 @@
-const books = localStorage.getItem('books') !== null
-  ? JSON.parse(localStorage.getItem('books'))
-  : [];
-
 const title = document.getElementById('title');
 const author = document.getElementById('author');
 const addBtn = document.getElementById('add-btn');
 let inputtedAuthor = '';
 let inputtedTitle = '';
+
+class Book {
+  constructor(title, author, bookId) {
+    this.title = title;
+    this.author = author;
+    this.bookId = bookId;
+  }
+
+  addBook = (title, author, bookId) => {
+    const newBook = new Book(title, author, bookId);
+    const ls =
+      localStorage.getItem('books') !== null
+        ? JSON.parse(localStorage.getItem('books'))
+        : [];
+    ls.unshift(newBook);
+    localStorage.setItem('books', JSON.stringify(ls));
+    console.log(localStorage);
+  };
+
+  removeBook = (bookId) => {
+    const ls = JSON.parse(localStorage.getItem('books'));
+    const removed = ls.filter((book) => book.bookId != bookId);
+    localStorage.setItem('books', JSON.stringify(removed));
+    console.log(localStorage);
+  };
+
+  displayBooks = () => {
+    if (localStorage.getItem('books') !== null) {
+      const lsBooks = JSON.parse(localStorage.getItem('books'));
+      lsBooks.forEach((element) => {
+        const li = document.createElement('li');
+        const pTitle = document.createElement('p');
+        const pAuthor = document.createElement('p');
+        const removeBook = document.createElement('button');
+        li.setAttribute('class', 'book-li');
+        pTitle.setAttribute('class', 'book-title');
+        pAuthor.setAttribute('class', 'book-author');
+        removeBook.setAttribute('onclick', `removeBook('${element.bookId}')`);
+        pTitle.innerHTML = `${element.title}`;
+        pAuthor.innerHTML = `${element.author}`;
+        removeBook.innerHTML = 'Remove';
+        li.innerHTML +=
+          pTitle.outerHTML + pAuthor.outerHTML + removeBook.outerHTML;
+        document.querySelector('.books-ul').appendChild(li);
+      });
+    }
+  };
+}
 
 title.addEventListener('input', (e) => {
   inputtedTitle = e.target.value;
